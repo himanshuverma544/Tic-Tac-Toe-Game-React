bf949086e2e7f0350203c5d99f82858b5e06b99a
Himanshu Verma <himanshuverma544@gmail.com>
2023-03-10 18:42:49 +0530

    update: implemented memoized function

--- src/functions.js
+++ src/functions.js
@@ -1,5 +1,26 @@
-export function highlightWinner(index, winnerSlots)
+function memoFunc(func)
+{
+  var cache = {};
+
+  return function (...args) {
+
+    const key = JSON.stringify(args);
+
+    if (key in cache) {
+      return cache[key];
+    }
+    else {
+      const result = func(...args);
+      cache[key] = result;
+      return result;
+    }
+  };
+}
+
+function highlightWinner(index, winnerSlots)
 {
   const [a, b, c] = winnerSlots;
   return (index === a || index === b || index === c) ? "success" : "warning";
 }
+
+export const memoHighlightWinner = memoFunc(highlightWinner);
